a5895d5a09c466a30092b6b74d0f62ff4eb7efff
impronen <impronen@gmail.com>
2022-12-21 15:12:00 +0200

    gameBoard object + boardSquares arrray
    gameBoard object contains an array of objects to represent the gameboard
    squares

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
+let gameBoard = {
+    //Array for gameboard squares
+    boardSquares: [
+        {
+            "ID": "A1",
+            "mark": 0,
+        },
+        {
+            "ID": "B1",
+            "mark": 0,  
+        },
+        {
+            "ID": "C1",
+            "mark": 0,  
+        },
+        {
+            "ID": "A2",
+            "mark": 0,  
+        },
+        {
+            "ID": "B2",
+            "mark": 0,  
+        },
+        {
+            "ID": "C2",
+            "mark": 0,  
+        },
+        {
+            "ID": "A3",
+            "mark": 0,  
+        },
+        {
+            "ID": "B3",
+            "mark": 0,  
+        },
+        {
+            "ID": "C3",
+            "mark": 0,  
+        },
+        
+    ]
+}
 
-
-let gameBoard = []
 
 
 const playerFactory = (name, mark) => {
     return { name, mark}
 }
+
+let player1 = playerFactory("Bob", "X");
+let player2 = playerFactory("Dylan", "O");
